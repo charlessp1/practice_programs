199a4ee108947e87b5bda503678eda661709e458
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 00:07:35 +0800

    Program complete-- added another method

--- sum_of_ten_nums.py
+++ sum_of_ten_nums.py
@@ -6,3 +6,10 @@ for i in range(1, 11):
     num = float(input(f"{i}. Enter a number: "))
     nums += num
 print(f"The sum of all ten numbers is {nums}")
+
+# Method 2:
+nums = []
+for i in range (1, 11):
+    num = float(input(f"{i}. Enter a number: "))
+    nums.append(num)
+print(f"The sum of all ten numbers is {sum(list(map(float, nums)))1}")
