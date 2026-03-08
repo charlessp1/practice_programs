b5409bd4773b6d1709206858cb0ae89e17166fb0
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 00:52:30 +0800

    Program complete-- added another method

--- print_odd_nums_from_10_inputs.py
+++ print_odd_nums_from_10_inputs.py
@@ -6,3 +6,7 @@ for i in range (1, 11):
     num = float(input(f"{i}. Enter a number: "))
     if num % 2 != 0: nums.append(num)
 print(f"{len(nums)} of the numbers you entered are odd numbers")
+
+# Method 2:
+nums = [float(input(f"{i}. Enter a number: ")) for i in range(1, 11)]
+print(f"{len([num for num in nums if num % 2 != 0])} of the numbers are odd numbers")
