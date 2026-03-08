449ee3b9e77c646fc9b1cf18ccc011dc33991b51
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-08 23:53:31 +0800

    Program complete-- one method used

---
+++ sum_of_ten_nums.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+#Method 1:
+nums = 0
+for i in range(1, 11):
+    num = float(input(f"{i}. Enter a number: "))
+    nums += num
+print(f"The sum of all ten numbers is {nums}")
