1bffc1a05b02139880bcfb6132d2831bbf8e46bb
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 17:05:01 +0800

    Program complete-- one method used

--- oddnums_0_to_100_using_while.py
+++ oddnums_0_to_100_using_while.py
@@ -1,8 +1,10 @@
 # Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 # Method 1:
+count = 0
 odds = []
-for num in range(0, 101):
-    if num % 2 != 0:
-        odds.append(num)
+while count < 100:
+    count += 1
+    if count % 2 != 0:
+        odds.append(count)
 print(*odds, sep=',')
