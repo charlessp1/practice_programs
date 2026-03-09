1eb5f4671c7cfa9e5bcd1af47f586f37ba1c17f3
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 16:03:13 +0800

    Program complete-- two methods used

--- firstnum_minus_nine_nums.py
+++ firstnum_minus_nine_nums.py
@@ -6,4 +6,9 @@ first_num = float(input("Enter first number: "))
 for i in range(9, 0, -1):
     num = float(input(f"Enter {i} more numbers: "))
     nums += num
-print(f"{first_num} minus nums is {first_num - nums}")
+print(f"{first_num} minus {nums} is {first_num - nums}")
+
+# Method 2
+first_number = float(input("Enter first number: "))
+nums = [float(input(f"Enter {i} more numbers: ")) for i in range(9, 0, -1)]
+print(f"{first_number} minus {sum(nums)} is {first_number - sum(nums)}")
