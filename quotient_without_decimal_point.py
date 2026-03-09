a2a1935cbf7ee0abc78e52ab0c2d70d1449857af
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 12:42:05 +0800

    Program complete-- added a method where it was rounded off

--- quotient_without_decimal_point.py
+++ quotient_without_decimal_point.py
@@ -6,3 +6,7 @@ num2 = float(input("Enter second number: "))
 
 no_decimal = num1 // num2
 print(f"The quotient of {num1} and {num2} without decimal points is {no_decimal}")
+
+# Method 2:
+nums = [float(input("Enter first number: ")), float(input("Enter second number: "))]
+print(f"The quotient of {nums[0]} and {nums[1]} rounded off is {round(nums[0] / nums[1])}")
