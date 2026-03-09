7d0a7f6bb37e5b9886cbd463a9cf50d0d2bd63ac
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 12:32:13 +0800

    Program complete-- one method used

---
+++ remainder_when_two_inputs_divided.py
@@ -0,0 +1,7 @@
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+
+# Method 1:
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"The remainder of {num1} divided by {num2} is {num1 % num2}")
