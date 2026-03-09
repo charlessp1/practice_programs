425ff66ed7d9a31509d41031ae5b6b72918e48eb
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 17:17:25 +0800

    Program complete-- two methods used

--- nums_not_ending_w_zero_and_five.py
+++ nums_not_ending_w_zero_and_five.py
@@ -3,6 +3,10 @@
 # Method 1:
 nums = []
 for num in range(101):
-    if num % 10 != 0 and num % 5 != 0:
+    if num % 10 and num % 5 != 0:
         nums.append(num)
+print(*nums, sep=',')
+
+# Method 2:
+nums = [num for num in range(101) if num % 10 and num % 5 !=0]
 print(*nums, sep=',')
