d37b6acb9b51410ab432fdfd173180f5bff6a051
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-09 17:28:50 +0800

    Program complete-- two methods used

--- nums_between_two_inputs.py
+++ nums_between_two_inputs.py
@@ -8,3 +8,8 @@ num2 = float(input("Enter second number: "))
 for num in range(int(num1) + 1, int(num2)):
     between.append(num)
 print(*between, sep=',')
+
+# Method 2:
+nums = [float(input("Enter first number: ")), float(input("Enter second number: "))]
+num_between = [num for num in range(int(nums[0]) + 1, int(nums[1]))]
+print(*num_between, sep=',')
