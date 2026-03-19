0c666a87b416d4e88ff22d2d62f5629342506ec7
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-19 16:39:09 +0800

    Program complete-- one method used

---
+++ infinite_input_display_sorted.py
@@ -0,0 +1,12 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+# Method 1:
+nums = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+        nums.append(num)
+        print(*sorted(nums), sep=',')
+    except ValueError:
+        print("Invalid input")
+        break
