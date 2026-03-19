ffaf49c191c4ca562c6f6c6179b9abbee8dd1ce4
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-19 23:16:24 +0800

    Test commit: No code yet

--- prog26_ten_input_display_nums_w_dups.py
+++
@@ -1,5 +0,0 @@
-# Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
-
-# Method 1:
-nums = [float(input(f"Enter {i} numbers: ")) for i in range(10, 0, -1)]
-print([num for num in nums if nums.count(num) == 1])
