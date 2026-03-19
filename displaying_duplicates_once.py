8c951b3d169e7611c87b757b84974acf7d9e7fd5
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-19 16:02:32 +0800

    Program complete-- one method used

--- displaying_duplicates_once.py
+++ displaying_duplicates_once.py
@@ -1,3 +1,5 @@
 # Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 #Method 1:
+nums = [float(input(f"Enter {i} numbers: ")) for i in range(10, 0, -1)]
+print(*dict.fromkeys(nums), sep=',')
