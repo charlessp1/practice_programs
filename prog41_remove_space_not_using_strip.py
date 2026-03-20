c15c1baf924776dc3ee3ab1abf7bb1eeb80bdcf3
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-20 14:56:59 +0800

    Program complete-- one method used

--- prog41_remove_space_not_using_strip.py
+++ prog41_remove_space_not_using_strip.py
@@ -1,3 +1,9 @@
 # lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 
 # Method 1:
+name = str(input("Enter your name with multiple spaces in front: "))
+for i in range(len(name)):
+    if name[i] != ' ':
+        name = name[i:]
+        break
+print(name)
