402a6b91fb1031eb057850de09bfe02ed46f45ce
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-20 13:01:00 +0800

    Program complete-- one method used

---
+++ prog32_nums_in_six_digit_format.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+# Method 1:
+name = str(input("Enter a number from 0 to 100: "))
+print(name.zfill(6))
