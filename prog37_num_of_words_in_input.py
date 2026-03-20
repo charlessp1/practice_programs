8b686bfcd0fca2a3bf5c7b269c7b40ea87afbbca
Charles Gian Esperanza <charlesgianesperanza@gmail.com>
2026-03-20 13:24:01 +0800

    Program complete-- one method used

--- prog37_num_of_words_in_input.py
+++ prog37_num_of_words_in_input.py
@@ -1,1 +1,5 @@
 # Create a program that ask the user to input a complete statement. Print the number of words in the input.
+
+#Method 1:
+statement = str(input("Enter a statement: "))
+print(len(statement.split()))
